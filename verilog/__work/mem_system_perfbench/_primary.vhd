e8d28ef6a7a044dd5303f4ff87e43f19132368c5
nambur <nambur@wisc.edu>
2013-04-25 17:27:11 -0500

    working two-way set-associative cache

--- verilog/__work/mem_system_perfbench/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity mem_system_perfbench is
-end mem_system_perfbench;
--- verilog/__work/mem_system_randbench/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity mem_system_randbench is
-end mem_system_randbench;
